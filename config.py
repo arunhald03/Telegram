d09100306be6eeb6c419917ac2bfac8eb7dd9d61
arunhald03 <111763149+arunhald03@users.noreply.github.com>
2023-05-08 13:41:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ if os.path.exists("local.env"):
 load_dotenv()
 class Config(object):
     log = True
-    APP_ID = getenv("API_ID", "11792103")
-    API_HASH = getenv("API_HASH", "5a03ee0c82de3e8972dd46a66e039b01")
+    APP_ID = getenv("API_ID", "28513073")
+    API_HASH = getenv("API_HASH", "726546a312f304da04a0e9b682db2abc")
     TOKEN = getenv("TOKEN", "2144094143:AAEbn55ufmFNwbuoXpdUQ5m_24ugEnXztd0")
     OWNER_ID = getenv("OWNER_ID", "868753606")
-    ASSISTANT_ID = getenv("ASSISTANT_ID", "5061573760")
+    ASSISTANT_ID = getenv("ASSISTANT_ID", "5356054197")
     STRING_SESSION = getenv("STRING_SESSION", "1BVtsOKEBu6R9GXb4YF1eoP6QZjVXcT4-dy2_9f0fzE2ebY-jOQgSCOuLLWWkfYjPP9FH1zWIb1MhCtXw4kEVVTaAaNwomXzanJS8IxMwjwgK6FFoKGQPflrN3E62nIWZg5_hT4caein953bzitAGQ1XKGsyBZRLgw-EAj7DoIGKfG9dyaqTBk9rPvdlK8E8JhSMr2VCVjdQJeZulDGbkV903EmN5SG0dA1bGRyVnVwAMJijX_wev9XTyHnmsp6G_Dfb71ZqtFGyY44XSDUBj_1v6Lx-LLJ30HSbXJs7313XIbkL9M59wKIpqEVY-hwzd_ufmkHg755RLVPURAjTpvz_5DW1moDA=") #telethon
     OWNER_USERNAME = getenv("OWNER_USERNAME", "smary_aruney")
     DB_URI = getenv("DATABASE_URL", "")
